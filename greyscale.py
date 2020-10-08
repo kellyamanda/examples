8880dc7c4976a77f2d22aed28bde42490a747697
Amanda Kelly <47222480+kellyamanda@users.noreply.github.com>
2020-10-07 20:27:05 -0700

    Create greyscale.py

---
+++ greyscale.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+
+image = st.file_uploader("Select an image!")
+
+col1, col2 = st.beta_columns(2)
+
+original = Image.open(image)
+col1.header("Original")
+col1.image(original, use_column_width=True)
+
+grayscale = original.convert('LA')
+col2.header("Grayscale")
+col2.image(grayscale, use_column_width=True)
